9d9a4344ecd600fc5636e3baeb6b8c553c24c26e
DewanShahriar <55458118+DewanShahriar@users.noreply.github.com>
2021-07-01 01:19:06 +0600

    site controller

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function index()
+    {
+        return view('frontend.site.index');
+    }
+
+    public function productDetail()
+    {
+        return view('frontend.site.productDetails');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('frontend.site.index');
+    return redirect('site');
+    //return view('frontend.site.index');
 });
 
-Route::get('/product-details', function () {
-    return view('frontend.site.productDetails');
-});
 
+Route::get('/', [App\Http\Controllers\SiteController::class, 'index'])->name('site');
+Route::get('/product-details', [App\Http\Controllers\SiteController::class, 'productDetail'])->name('site.product.detail');
 
 Auth::routes();
 
