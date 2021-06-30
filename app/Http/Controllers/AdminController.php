2c3e82a4a2c289273d5a94aaf2d0ebfe94bf2a72
DewanShahriar <55458118+DewanShahriar@users.noreply.github.com>
2021-07-01 00:47:30 +0600

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\File; 
+use Illuminate\Support\Facades\File;
 use App\Models\User;
 use Image;
 use DB;
